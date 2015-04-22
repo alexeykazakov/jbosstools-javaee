dcb85750529f90d6cfda6f0e89c33a21c1dcfb62
Tomas Milata <tomas.milata@gmail.com>
2015-04-22 18:19:29 +0200

    Overriden standard diagram layout persistence service. (Auto layout is used on load).

---
+++ batch/plugins/org.jboss.tools.batch.ui/src/org/jboss/tools/batch/ui/editor/internal/services/BatchDiagramLayoutPersistenceService.java
@@ -0,0 +1,13 @@
+package org.jboss.tools.batch.ui.editor.internal.services;
+
+import org.eclipse.sapphire.ui.diagram.DiagramConnectionPart;
+import org.eclipse.sapphire.ui.diagram.layout.DiagramLayoutPersistenceService;
+
+public class BatchDiagramLayoutPersistenceService extends DiagramLayoutPersistenceService {
+
+	@Override
+	public DiagramConnectionInfo read(DiagramConnectionPart connection) {
+		return null;
+	}
+
+}
